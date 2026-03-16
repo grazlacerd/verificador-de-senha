5806c9449aa7ad85ab8346554979338a5607aca9
Grazielly Lacerda <graziilacerda19@gmail.com>
2026-03-15 21:25:22 -0300

    Refactor password validation messages in testarSenha

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ let mensagem = document.getElementById("mensagem");
         mensagem.innerText = `Senha muito curta! Use pelo menos 8 caracteres.`;
     } else if (senhaInput == '12345678' ) {
         mensagem.style.color = "#ff4d4d";
-         mensagem.innerText = `Essa senha é muito manjada, parceiro! Troca isso aí.`;
+         mensagem.innerText = `Essa senha é muito manjada! Escolha uma forte.`;
     } else {
         mensagem.style.color = "#00ff66";
-        mensagem.innerText = `mostre uma mensagem em verde: 'Senha forte! Cadastro liberado.`;
+        mensagem.innerText = `Senha forte! Cadastro liberado.`;
     };
 
 }
